80f5bb5ec973d6b292b6a5e1b69f954b284c3d85
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2022-12-11 17:09:01 +0000

    Fix all the code for loading/saving json

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
+import random
 
-# TODO: Get menu system from Replit
+
+def generate_id(bits=64):
+    return random.randint(0, 2**bits)
 
 
 def create_child(name: str, score: int, postcode: str, id: str):
@@ -23,14 +26,27 @@ def register_new_child():
         postcode = input("Postcode: ")
         score = int(input("Total score: "))
 
-        return create_child(name, score, postcode)
+        id = generate_id()
+        return create_child(name, score, postcode, id)
 
     print("Registering a new child")
     print("Please input their information's")
     child = ask_for_data()
+    child_database.append(child)
+    save_json()
 
 
-child_database_file = open("children.json", "w")
-child_database = json.load(child_database_file)
+child_database_filename = "children.json"
+# Load the JSON database file:
+# TODO: Create the file if it doesn't exist
+child_database_file_read = open(child_database_filename, "r", encoding="utf8")
+child_database: list = json.load(child_database_file_read)
+child_database_file_read.close()
+# Open the database file for writing:
+child_database_file = open(child_database_filename, "w", encoding="utf8")
 
+# TODO: Get menu system from Replit
 register_new_child()
+
+# Program shutdown
+child_database_file.close()

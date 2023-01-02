32ac1c2bf2391793b801930d9955fcc646330101
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2023-01-02 21:17:13 +0000

    Make sure it's spec-compliant

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@ PROGRAM_NAME = "Christmas Naughty or Nice"
 
 # Filename constants
 FILE_CHILD_DATABASE: str = "children.json"
-FILE_NAUGHTY_LIST = "naughty_list.txt"
-FILE_NICE_LIST = "nice_list.txt"
+FILE_NAUGHTY_LIST = "Naughty.txt"
+FILE_NICE_LIST = "Nice.txt"
 
 # Folder constants
 FOLDER_LETTERS = "letters"
